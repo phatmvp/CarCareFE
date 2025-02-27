f051ddb55d1376814714152e1f83794ee12111d1
Sown <hoagsonn3@gmail.com>
2025-02-27 16:08:17 +0700

    Login,Home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import HomePage from "./component/HomePage/homePage";
-
+import HomePage from "./component/HomePage/HomePage";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import LoginPage from "./pages/Login";
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+
+
+
+        <Route path="/" element={<Navigate to="/car-care/home" />} />
+        <Route
+          path="/car-care/home"
+          element={
+            <HomePage />
+          }
+        />
+        <Route
+          path="/car-care/login"
+          element={
+            <LoginPage />
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
